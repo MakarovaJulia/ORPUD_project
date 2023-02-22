5d2671ad18630c91edf920a015ba9e5894d6845f
Макарова Юлия Андреевна <julmakan@gmail.com>
2023-02-22 11:17:20 +0300

    added user registration

--- web/views.py
+++ web/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
 
 from web.forms import RegistrationForm
 
+User = get_user_model()
 
 def main_view(request):
     return render(request, "web/main.html")
@@ -9,4 +11,15 @@ def main_view(request):
 
 def registration_view(request):
     form = RegistrationForm()
-    return render(request, "web/registration.html", {"form": form})
+    is_success = False
+    if request.method == 'POST':
+        form = RegistrationForm(data=request.POST)
+        if form.is_valid():
+            user = User(
+                username=form.cleaned_data['username'],
+                email=form.cleaned_data['email']
+            )
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            is_success = True
+    return render(request, "web/registration.html", {"form": form, "is_success": is_success})
