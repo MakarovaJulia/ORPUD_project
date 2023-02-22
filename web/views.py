d98e1a300cef05a6374ef23d9152e526da941efa
Макарова Юлия Андреевна <julmakan@gmail.com>
2023-02-22 10:46:07 +0300

    added main and registration views

--- web/views.py
+++ web/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from web.forms import RegistrationForm
+
+
+def main_view(request):
+    return render(request, "web/main.html")
+
+
+def registration_view(request):
+    form = RegistrationForm()
+    return render(request, "web/registration.html", {"form": form})
